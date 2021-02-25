315aff34db9b5a4a10e05c76a0dbd12ba9130a36
Ben <bengiftakis@gmail.com>
2021-02-25 18:45:47 -0500

    i hate c still

--- Spring 2021/Comp Org/Lecture/MP1/cmds.c
+++ Spring 2021/Comp Org/Lecture/MP1/cmds.c
@@ -33,18 +33,18 @@
 
 PROTOTYPE int stop(Cmd *cp, char *arguments);
 PROTOTYPE int mem_display(Cmd *cp, char *arguments);
-//PROTOTYPE int mem_set(Cmd *cp, char *arguments);
-//PROTOTYPE int help(Cmd *cp, char *arguments);
+PROTOTYPE int mem_set(Cmd *cp, char *arguments);
+PROTOTYPE int help(Cmd *cp, char *arguments);
 
 /* command table */
 
 Cmd cmds[] = {{"md",  mem_display, "Memory display: MD <addr>"},
               {"s",   stop,        "Stop" },
-//             {"ms",  mem_set,     "Memory set: ms <addr> <value>" },
-//              {"h",   help,        "Help <cmd>" },
+              {"ms",  mem_set,     "Memory set: ms <addr> <value>" },
+              {"h",   help,        "Help <cmd>" },
               {NULL,  NULL,        NULL}};  /* null cmd to flag end of table */
 
-char xyz = 97;  /* test global variable  */
+char xyz = 6;  /* test global variable  */
 char *pxyz = &xyz;  /* test pointer to xyz */
 /*===================================================================*
 *		command			routines
@@ -66,8 +66,7 @@ int stop(Cmd *cp, char *arguments)
 *
 */
 
-int mem_display(Cmd *cp, char *arguments)
-{
+int mem_display(Cmd *cp, char *arguments){
 	int location;
 	unsigned char *pointer; //strange overflow glitches if i leave this as a normal char
 	sscanf(arguments, "%x", &location); //convet string to a hex and store it
@@ -78,6 +77,7 @@ int mem_display(Cmd *cp, char *arguments)
 	//printf("xyz location: %p\n", &xyz);
 	//printf("%02x, ", *pointer);
 	//above are some debugging prints because i found gdb useless here
+	printf("%s\t", arguments);
 	for(int i = 0; i<16; i++){
 		printf("%02x ", *(pointer + i)); //the 02 forces leading zeroes to format, x is for hex (lowercase)
 	}
@@ -89,5 +89,34 @@ int mem_display(Cmd *cp, char *arguments)
 		else{ printf(". ");}
 	}
 	printf("\n");//formatting
-	return 0;
-}
+	return 0;}
+int mem_set(Cmd *cp, char *arguments){
+	int location, value;
+	unsigned char *pointer; //i still dont really know the nuances of this but im just going to use unsigned char because it caused a problem in md
+	sscanf(arguments, "%x %x", &location, &value); //parse args
+	pointer = location;
+//	printf("detected address: %x\n", &location);
+//	printf("value at address: %02x\n", *pointer);
+//	printf("value to insert: %02x\n", value);
+//	printf("setting value...\n");
+	*pointer = value; //set value
+//	printf("value at address: %02x\n", *pointer);
+//the prints were debugging stuff because i still feel like gdb is not nearly as usable as a debugger in a real ide that will show my every variable line by line during execution
+return 0;}
+int help(Cmd *cp, char *arguments){
+//	printf("command to eval:%s\n", arguments);
+	if(strcmp(" ms", arguments) == 0){ //compares string to arg
+		printf("%s\n", cmds[2].help);//grabs from location from the cmd table, this could be more efficient this works and ive spent 20 hours on this project so this will have to do
+	}
+	else if(strcmp(" md", arguments) == 0){
+		printf("%s\n", cmds[0].help);
+	}
+	else if(strcmp(" h", arguments) == 0){
+		printf("%s\n", cp->help);
+	}
+	else if(strcmp(" s", arguments) == 0){
+		printf("%s\n", cmds[1].help);
+	}
+	else{ printf("couldnt find any matches, your code is broke or you didn't actually type a valid command");}
+return 0;}
+
